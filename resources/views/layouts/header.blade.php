6a08d129cb4116d72db3df1a51a576136cf04126
andri <andrinett@gmail.com>
2021-07-13 01:34:08 +0700

    modify misc

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -89,7 +89,7 @@
     <!-- Brand Logo -->
     <a href="{{route('dashboard')}}" class="brand-link">
       
-      <span class="brand-text font-weight-light">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ewaporBappeda</span>
+      <span class="brand-text font-weight-light">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mail Relay</span>
     </a>
 
     <!-- Sidebar -->
